b4321ab5796bae8064582cd31054153151349edd
Marin Cabac <marin.cabac@gmail.com>
2018-02-14 11:55:54 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-		 
+
 
 		 #	:yukihiro_matsumoto =>
 		 #	 :known_for => "Ruby",
@@ -108,7 +108,8 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
+programmer_hash[:alan_kay][:known_for]= "GUI"
+		 #alans_new_info = "GUI"
 
 
 end
